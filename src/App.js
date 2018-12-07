9f33f720944b55fea96a0fc4daf5d577485da3ba
Vedrana <vedrana_ravlic16@yahoo.com>
2018-12-07 21:55:56 +0100

    created header and contact components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="My contacts" />
+        <Contact
+          name="Jules Verne"
+          email="jverne@gmail.com"
+          phone="123-456-789"
+        />
+        <Contact
+          name="Mary Shelley"
+          email="mshelley@gmail.com"
+          phone="010-456-789"
+        />
       </div>
     );
   }
