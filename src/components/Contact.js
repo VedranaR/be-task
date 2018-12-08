ff2f72cb38d5b9561c312d0c3c02d6ed1778e765
Vedrana <vedrana_ravlic16@yahoo.com>
2018-12-08 20:20:01 +0100

    added FA icon to the cards

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,9 @@ class Contact extends Component {
     const { name, email, phone } = this.props;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name} <i className="fas fa-magic" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
