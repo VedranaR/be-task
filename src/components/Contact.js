c0f494d23b306da6e917f4e1eacead3d663eb84e
Vedrana <vedrana_ravlic16@yahoo.com>
2018-12-15 15:37:24 +0100

    added input text func

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,81 +5,75 @@ class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cardColorBlack: "#000000",
-      colorIsBlack: true,
-      editContent: false,
+      addContent: false,
       randomColors: [],
-      randomColor: ""
+      comment: "",
+      style: {
+        color: "black"
+      }
     };
-  }
 
-  componentDidMount() {
-    fetch("http://www.colr.org/json/color/random/")
+    fetch("http://www.colr.org/json/color/random")
       .then(response => response.json())
       .then(data => {
         this.state.randomColors[0] = "#" + data.new_color;
       });
 
-    fetch("http://www.colr.org/json/color/random/")
+    fetch("http://www.colr.org/json/color/random")
       .then(response => response.json())
       .then(data => {
         this.state.randomColors[1] = "#" + data.new_color;
       });
   }
 
-  getRandomColor = e => {
-    let col = Math.floor(Math.random() * 2);
-    this.setState({ randomColor: this.state.randomColors[col] });
+  getRandomColor = () => {
+    if (this.state.style.color === "black") {
+      let col = Math.floor(Math.random() * 2);
+      this.setState({ style: { color: this.state.randomColors[col] } });
+    } else {
+      this.setState({ style: { color: "black" } });
+    }
   };
 
-  changeColor = e => {
-    this.setState({ colorIsBlack: !this.state.colorIsBlack });
+  toggleInput = () => {
+    this.setState({ addContent: !this.state.addContent });
   };
 
-  toggleInput = e => {
-    this.setState({ editContent: !this.state.editContent });
+  changeInput = e => {
+    this.setState({ comment: e.target.value });
   };
 
   render() {
     const { name, email, phone } = this.props;
-    const { cardColorBlack } = this.state;
-    const { colorIsBlack } = this.state;
-    const { editContent } = this.state;
-    const { randomColor } = this.state;
+    const { addContent } = this.state;
 
     return (
       <div
         className="card card-body mb-3"
-        onClick={(this.getRandomColor, this.changeColor)}
-        style={colorIsBlack ? { cardColorBlack } : { randomColor }}
+        onClick={this.getRandomColor}
+        style={this.state.style}
       >
-        <h4>
-          {name}
-          <i
-            className="fas fa-user-edit"
-            style={{ cursor: "pointer", float: "right" }}
-            onClick={this.toggleInput}
-          />
-        </h4>
+        <h4>{name}</h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
-        </ul>
-        {editContent ? (
-          <div className="form-group">
-            <label htmlFor="comment">Edit email</label>
-            <input
-              type="text"
-              name="email"
-              className="form-control form-control-sm"
-              placeholder={email}
+          <li className="list-group-item">
+            Comment: {this.state.comment}
+            <i
+              className="fas fa-user-edit"
+              style={{ cursor: "pointer", float: "right" }}
+              onClick={this.toggleInput}
             />
-            <label htmlFor="comment">Edit phone</label>
+          </li>
+        </ul>
+        {addContent ? (
+          <div className="form-group">
+            <label htmlFor="comment">Add comment</label>
             <input
               type="text"
-              name="phone"
               className="form-control form-control-sm"
-              placeholder={phone}
+              placeholder="write a comment"
+              onInput={this.changeInput}
             />
           </div>
         ) : null}
@@ -87,14 +81,6 @@ class Contact extends Component {
     );
   }
 }
-
-// const cardColorDefault = {
-//   color: "black"
-// };
-
-// const cardColorRandom = {
-//   color: cardColorTwo
-// };
 
 Contact.propTypes = {
   name: PropTypes.string.isRequired,
